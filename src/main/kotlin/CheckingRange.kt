368ced938232dda9107b7804a48230f651ffc08e
pcouqc <nagapen@nate.com>
2024-04-30 17:19:22 +0900

    업로딩

--- src/main/kotlin/CheckingRange.kt
+++ src/main/kotlin/CheckingRange.kt
@@ -2,13 +2,9 @@ package org.example
 
 class CheckingRange : Checking() {
     override fun check(a: IntArray): Boolean {
-        if (a[0] == 0) {
+        if (a[0] == 0 || a[0] != a[1] || a[0] != a[2] || a[1] != a[2]) {
             return false
-        }
-        if (a[0] != a[1] || a[0] != a[2] || a[1] != a[2]) {
-            return false
-        }
-        else {
+        } else {
             return true
         }
     }
