0bc6e07d7bb46e7258f9622e616a04f84a053aad
pcouqc <nagapen@nate.com>
2024-05-02 18:53:47 +0900

    리리펙토링

--- src/main/kotlin/Number/MakingNumber.kt
+++ src/main/kotlin/Number/MakingNumber.kt
@@ -6,7 +6,7 @@ class MakingNumber {
 
     fun makeNumber(): IntArray {
         while (true) {
-            val random = Random.nextInt(100, 1000).toString().map { it.toInt() }.toIntArray()
+            val random = Random.nextInt(100, 1000).toString().map { it.code }.toIntArray()
             if (random.size == 3 && random[0] != 0 && random[0] != random[1] && random[0] != random[2] && random[1] != random[2]) {
                 return random
             }
